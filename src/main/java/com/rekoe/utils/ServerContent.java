a5bc2081063f75c25427f5be2a0a767586522d06
rekoe <koukou890@qq.com>
2014-02-28 11:09:55 +0800

    test Code

---
+++ src/main/java/com/rekoe/utils/ServerContent.java
@@ -0,0 +1,10 @@
+package com.rekoe.utils;
+
+public class ServerContent {
+	public static final String CLIENT_ID = "someclientid";
+	public static final String CLIENT_SECRET = "someclientsecret";
+	public static final String ISSUED_AT = "0123456789";
+	public static final Long EXPIRES_IN = 987654321l;
+
+	public static final String ACCESS_TOKEN = "access_token_valid";
+}
