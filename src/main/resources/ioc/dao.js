02090d8383ffab3d10f1db4ed117db25c4796a3f
wendal <wendal1985@gmail.com>
2015-01-18 18:24:07 +0800

    change: jdbc配置改成独立的配置文件

--- src/main/resources/ioc/dao.js
+++ src/main/resources/ioc/dao.js
@@ -1,4 +1,10 @@
 var ioc = {
+	dbconf: {
+		type : "org.nutz.ioc.impl.PropertiesProxy",
+		fields: {
+			paths : "jdbc.properties"
+		}
+	},
 	dataSource : {
 		type : "com.alibaba.druid.pool.DruidDataSource",
 		events : {
@@ -6,10 +12,10 @@ var ioc = {
 		},
 		fields : {
 			// 请修改下面的数据库连接信息
-			url : 'jdbc:mysql://127.0.0.1:3306/platform?useUnicode=true&characterEncoding=utf-8',
-			username : 'root',
-			password : 'root',
-			maxActive : 20,
+			url : {java:"$dbconf.get('db.url', 'jdbc:mysql://127.0.0.1:3306/platform?useUnicode=true&characterEncoding=utf-8')"},
+			username : {java:"$dbconf.get('db.username', 'root')"},
+			password : {java:"$dbconf.get('db.password', 'root')"},
+			maxActive : {java:"$dbconf.getInt('db.maxActive', 20)"},
 			validationQuery : "SELECT 'x'",
 			testWhileIdle : true,
 			testOnBorrow : false,
