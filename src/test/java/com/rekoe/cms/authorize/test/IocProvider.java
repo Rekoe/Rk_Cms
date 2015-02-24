a58655041f85a8379a0e7495fbd1eddf34d51dee
rekoe <koukou890@qq.com>
2015-02-24 14:01:46 +0800

    add

---
+++ src/test/java/com/rekoe/cms/authorize/test/IocProvider.java
@@ -0,0 +1,47 @@
+package com.rekoe.cms.authorize.test;
+
+import java.util.List;
+
+import org.nutz.dao.Dao;
+import org.nutz.ioc.Ioc;
+import org.nutz.ioc.impl.NutIoc;
+import org.nutz.ioc.loader.combo.ComboIocLoader;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
+
+import com.rekoe.crawler.core.constants.PropertiesCrawlerConfig;
+import com.rekoe.domain.CrawlerRule;
+
+/**
+ * TODO 实际项目中获取ioc的方式可灵活决定
+ * 
+ */
+public class IocProvider {
+	private static Log logger = Logs.get();
+
+	private static Ioc ioc;
+
+	public static void init() {
+		try {
+			ioc = new NutIoc(new ComboIocLoader("*org.nutz.ioc.loader.json.JsonLoader", "ioc/datasource.js", "*org.nutz.ioc.loader.annotation.AnnotationIocLoader", "com.rekoe.domain"));
+			new PropertiesCrawlerConfig().loadCrawlerConfig("GatherCore.properties");
+		} catch (ClassNotFoundException e) {
+			logger.error("Ioc create error", e);
+		} catch (Exception e) {
+			logger.error("Ioc create error", e);
+		}
+	}
+
+	public static Ioc ioc() {
+		if (ioc == null) {
+			init();
+		}
+		return ioc;
+	}
+
+	public static void main(String[] args) {
+		Dao dao = ioc().get(Dao.class);
+		List<CrawlerRule> list = dao.query(CrawlerRule.class, null);
+		System.out.println(list);
+	}
+}
