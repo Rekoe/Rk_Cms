7df34fde50aa6e372b30e0693c5d97f5e65a3a1c
kouxian <koukou890@qq.com>
2015-05-15 09:56:36 +0800

    添加 daocache

--- src/main/resources/ioc/dao.js
+++ src/main/resources/ioc/dao.js
@@ -14,10 +14,10 @@ var ioc = {
 	dataSource : {
 		type : "com.alibaba.druid.pool.DruidDataSource",
 		events : {
+			create : "init",
 			depose : 'close'
 		},
 		fields : {
-			// 请修改下面的数据库连接信息
 			url : {
 				java : "$dbconf.get('db.url', 'jdbc:mysql://127.0.0.1:3306/platform?useUnicode=true&characterEncoding=utf-8')"
 			},
@@ -33,14 +33,43 @@ var ioc = {
 			validationQuery : "SELECT 'x'",
 			testWhileIdle : true,
 			testOnBorrow : false,
-			testOnReturn : false
+			testOnReturn : false,
+			filters : "mergeStat",
+			connectionProperties : "druid.stat.slowSqlMillis=1000"
 		}
 	},
 
 	dao : {
-		type : 'org.nutz.dao.impl.NutDao',
+		type : "org.nutz.dao.impl.NutDaoExt",
 		args : [ {
-			refer : 'dataSource'
-		} ]
+			refer : "dataSource"
+		} ],
+		fields : {
+			executor : {
+				refer : "cacheExecutor"
+			}
+		}
+	},
+	cacheExecutor : {
+		type : "org.nutz.plugins.cache.dao.CachedNutDaoExecutor",
+		fields : {
+			cacheProvider : {
+				refer : "cacheProvider"
+			},
+			cachedTableNames : [ "article", "article_category", "acquisition",
+					"crawler_rule", "system_permission", "permission_category",
+					"system_role", "system_setting" ],
+			enableWhenTrans : false, // 事务作用域内不启用缓存,默认也是false
+			db : "MYSQL"
+		}
+	},
+	cacheProvider : {
+		type : "org.nutz.plugins.cache.dao.impl.provider.MemoryDaoCacheProvider",
+		fields : {
+			cacheSize : 10000000
+		},
+		events : {
+			create : "init"
+		}
 	}
 };
